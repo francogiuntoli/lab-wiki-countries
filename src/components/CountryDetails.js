07919783a31a762cf92b2e401217da10826dfbf9
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-07 10:06:26 +0200

    refactored code after done

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import {Link, Routes} from 'react-router-dom'
-
-
 export default class CountryDetails extends Component {
-
+    
     state={
         countryDetails:null,
     }
     
-    
      fetchData = () => {
         let countryId = String(this.props.match.params.id)
         axios.get(`https://restcountries.eu/rest/v2/alpha/${countryId}`)
-   
         .then((result) => {
             const {name, capital, area, borders} = result.data
             let detail={
@@ -33,34 +29,23 @@ export default class CountryDetails extends Component {
     componentDidMount(){
         this.fetchData()
     }
-    
-        
+       
     componentDidUpdate(){
         let paramsId = this.props.match.params.id
         this.state.countryDetails.borders.filter((e)=>{
             if(paramsId == e){
                 return this.fetchData()
             }
-
         })        
     }
-  
-        
-    
-    
-
-
 
     render() {
         
         const {countryDetails} = this.state
-        
         if(!countryDetails){
             return <h1>Loading...</h1>
         }
-    
-        
- 
+
         return (
             <div>
                 <h1>{countryDetails.name}</h1>
@@ -81,7 +66,7 @@ export default class CountryDetails extends Component {
                 <tr>
                   <td>Borders</td>
                   <td>
-                        {                          
+                        {       
                             countryDetails.borders.map((e, i)=>{  
                                 return <div key={i}>
                                     <ul>
@@ -92,7 +77,6 @@ export default class CountryDetails extends Component {
                                 </div>
                             })
                         }
-
                   </td>
                 </tr>
               </tbody>
